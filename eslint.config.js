f03435043b059c4e427c46dc8c3812c80501b0af
araragi-lacking-branding <187688377+araragi-lacking-branding@users.noreply.github.com>
2025-11-13 15:51:27 -0500

    Add ESLint for code quality checks and lint script

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      // Add custom rules here
+    },
+    ignores: ['src/static/app.js'],
+  },
+];
